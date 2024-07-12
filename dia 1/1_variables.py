6137259a5384e233d942ef702e3490eeed9296f1
Giancarlo Reynafarje Alocén <gcreynafarje@gmail.com>
2024-07-11 21:23:59 -0500

    inicios en python

---
+++ dia 1/1_variables.py
@@ -0,0 +1,38 @@
+nombre = 'Giancarlo'
+
+edad = 32
+
+soltero = True # Boolean > solamente o es True o es False
+
+sueldo = 870.35
+
+# Tipos de datos PRIMITIVOS
+
+nombre = -1
+
+#En python los tipos de variables puecden cambiar sin ninguna restriccion 
+
+# java | c# | c++ no se puede cambiar el tipo de dato una vez creado
+
+# Cada tipo de dato de las variables tienen sus propiedades y metodos definidos
+
+# Las variables siempre deben empezar con letras
+
+texto1 = 'Hola yo me llamo'
+texto2 = 'Giancarlo'
+
+resultado = texto1 + texto2
+
+print(resultado)
+
+alumno1, alumno2, alumno3 = 'Ignacio', 'Segundo', 'Abel'
+
+print(alumno1)
+
+print(alumno1.upper())
+
+print(edad.upper())
+
+# split > separar el texto en base al caracter de busqueda
+resultado = texto1.split(" ")
+print(resultado)
