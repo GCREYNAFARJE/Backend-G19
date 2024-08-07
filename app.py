3c25ec9cd831d28a2c30afb65305b3ade7c7041e
Giancarlo Reynafarje Alocén <gcreynafarje@gmail.com>
2024-08-07 13:17:35 -0500

    uso de flask resful y creacion de controladores

--- app.py
+++ app.py
@@ -4,16 +4,24 @@ from dotenv import load_dotenv
 from os import environ
 from models import *
 from flask_migrate import Migrate
-
+from controllers import *
+# API > Application Program Interface
+from flask_restful import Api
+# busca el archivo .env y cargara las variables como si fueran variables de entorno 
 load_dotenv()
 
 app = Flask(__name__)
-print(environ.get('DATABASE_URL2'))
+
+# Definimos la API de nuestra aplicacion de Flask
+api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI']=environ.get('DATABASE_URL2')
 
 conexion.init_app(app)
 
 Migrate(app,conexion)
 
+# Agregamos los recursos (controladores a nuestra API)
+api.add_resource(CategoriasController, '/categorias')
+api.add_resource(CategoriaController,'/categoria/<int:id>')
 if __name__ == '__main__':
     app.run(debug=True)
