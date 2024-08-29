e7d0acbceab105b7d1242cf46adf3e55e5517786
Giancarlo Reynafarje Alocén <gcreynafarje@gmail.com>
2024-08-28 22:28:03 -0500

    avance

---
+++ 1_intro_node/ecmascript/1_repaso.mjs
@@ -0,0 +1,22 @@
+import {restar} from './2_funciones.mjs'
+
+let x = 'Giancarlo'
+
+x = 'ramiro'
+x = 'Juan'
+x = 20
+console.log('Hola')
+
+
+function sumar(numero1, numero2) {
+    const resultado = numero1 + numero2
+
+    return resultado
+}
+
+const sumatoria = sumar(10,20)
+
+console.log(sumatoria)
+
+const resta = restar(50,30)
+console.log(resta)
