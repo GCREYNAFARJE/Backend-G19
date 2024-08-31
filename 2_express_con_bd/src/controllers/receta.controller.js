10d856ca749f88a8171ad75959f837510ade77ff
Giancarlo Reynafarje Alocén <gcreynafarje@gmail.com>
2024-08-31 03:42:53 -0500

    uso de prisma con express

--- 2_express_con_bd/src/controllers/receta.controller.js
+++ 2_express_con_bd/src/controllers/receta.controller.js
@@ -1,6 +1,18 @@
+import { prisma } from '../cliente.js';
 
-export function crearReceta (req, res){
+export async function crearReceta(req, res) {
+    // Body del request
+    const body = req.body; // {nombre: 'Tres leches de vainilla' }
+    const resultado = await prisma.receta.create({
+        data: {
+            nombre: body.nombre,
+            descripcion: body.descripcion,
+        },
+    });
+
+   
     return res.json({
-        message:'Receta creada exitosamente'
-    })
+        message:"Receta creada exitosamente",
+        content: resultado, 
+    });
 }
